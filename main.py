89ab721087e18e6af2509362e410696bde807087
Fabrizio Dante Pauselli <fabriziopauselli@gmail.com>
2020-05-23 13:52:53 -0300

    Uruguay

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from WebPages.Selenium.SpainPage import SpainPage
 from WebPages.PortugalPage import PortugalPage
 from WebPages.ArgentinaPage import ArgentinaPage
 from WebPages.BrazilPage import BrazilPage
+from WebPages.UruguayPage import UruguayPage
 
 if __name__ == "__main__":
-    scrapper = Scrapper(PortugalPage())
+    scrapper = Scrapper(UruguayPage())
     articles = scrapper.articles()
