024fee955fe917fe2b95c73de520a8b59b063a25
Fabrizio Dante Pauselli <fabriziopauselli@gmail.com>
2020-05-23 11:31:57 -0300

    Portugal

---
+++ WebPages/Articles/PortugalArticle.py
@@ -0,0 +1,39 @@
+import bs4
+import requests
+
+from Helpers.FileHelper import FileHelper
+from WebPages.Articles.GenericArticle import GenericArticule
+from WebPages.Articles.GenericArticle import replace_new_line_and_tab
+
+
+class PortugalArticle(GenericArticule):
+    fileHelper: FileHelper
+
+    def __init__(self, article_url: str, file_helper: FileHelper):
+        super().__init__(article_url, file_helper)
+        self.date = '.date-display-single'
+        self.text = '.field.field-name-body.field-type-text-with-summary.field-label-hidden'
+        self.title = 'page-header'
+        res = requests.get(self.url)
+        res.raise_for_status()
+        self.soup = bs4.BeautifulSoup(res.text, features="html.parser")
+
+    def get_title(self):
+        title = self.soup.find('div', {'class': self.title}).getText()
+        return replace_new_line_and_tab(title)
+
+    def get_date(self):
+        date = self.soup.select('.published')
+        date = date[0]
+        date = date.find('time')
+        date = date.attrs['datetime']
+        return replace_new_line_and_tab(date)
+
+    def get_text(self):
+        paragraph = self.soup.find('div', {'itemprop': 'articleBody'})
+        output = ''
+        text = paragraph.find_all('p')
+        for p in text:
+            paragraph = "{} (newline) ".format(replace_new_line_and_tab(p.getText()))
+            output = output + paragraph
+        return output
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 from Scrapper import Scrapper
 from WebPages.Selenium.SpainPage import SpainPage
+from WebPages.PortugalPage import PortugalPage
 from WebPages.ArgentinaPage import ArgentinaPage
 from WebPages.BrazilPage import BrazilPage
 
 if __name__ == "__main__":
-    scrapper = Scrapper(SpainPage())
+    scrapper = Scrapper(PortugalPage())
     articles = scrapper.articles()
