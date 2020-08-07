8fca60eaff5c8d5242a194e1e0cb14a0700581e3
Fabrizio Dante Pauselli <fabriziopauselli@gmail.com>
2020-08-07 19:47:13 -0300

    Chile

---
+++ WebPages/Articles/ChileArticle.py
@@ -0,0 +1,36 @@
+import bs4
+import requests
+
+from Helpers.FileHelper import FileHelper
+from WebPages.Articles.GenericArticle import GenericArticle
+from WebPages.Articles.GenericArticle import replace_new_line_and_tab
+
+
+class ChileArticle(GenericArticle):
+    _file_helper: FileHelper
+
+    def __init__(self, article_url: str, file_helper: FileHelper):
+        super().__init__(article_url, file_helper)
+        self._date = 'fecha'
+        self._text = 'CUERPO'
+        self._title = 'titular'
+        res = requests.get(self._url)
+        res.raise_for_status()
+        self._soup = bs4.BeautifulSoup(res.text, features="html.parser")
+
+    def _get_title(self):
+        title = self._soup.find('div', {'class': self._title}).getText()
+        return replace_new_line_and_tab(title)
+
+    def _get_date(self):
+        date = self._soup.find('span', {'class': self._date}).getText()
+        return replace_new_line_and_tab(date)
+
+    def _get_text(self):
+        paragraph = self._soup.find('div', {'class': self._text})
+        output = ''
+        text = paragraph.find_all('p')
+        for p in text:
+            paragraph = "{} (newline) ".format(replace_new_line_and_tab(p.getText()))
+            output = output + paragraph
+        return output
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 from Scrapper import Scrapper
+from WebPages.ChilePage import ChilePage
 from WebPages.ColombiaPage import ColombiaPage
 from WebPages.Selenium.SpainPage import SpainPage
 from WebPages.PortugalPage import PortugalPage
 from WebPages.ArgentinaPage import ArgentinaPage
 from WebPages.BrazilPage import BrazilPage
 from WebPages.UruguayPage import UruguayPage
+from WebPages.ParaguayPage import ParaguayPage
 
 if __name__ == "__main__":
-    scrapper = Scrapper(ColombiaPage())
+    scrapper = Scrapper(ChilePage())
     articles = scrapper.articles()
